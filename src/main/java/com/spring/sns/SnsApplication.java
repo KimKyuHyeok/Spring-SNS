4b9a7ed885924a36b1fbe96cb29da55037f17557
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-18 12:42:25 +0900

    initialize project

--- src/main/java/com/spring/sns/SnsApplication.java
+++ src/main/java/com/spring/sns/SnsApplication.java
@@ -2,11 +2,13 @@ package com.spring.sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SnsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SnsApplication.class, args);
 	}
 
