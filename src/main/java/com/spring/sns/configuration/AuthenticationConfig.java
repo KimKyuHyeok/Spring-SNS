93b435b27ee508a6e0c793b1df87cef23443593a
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-24 13:50:55 +0900

    feat: jwt filter 작성

--- src/main/java/com/spring/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/spring/sns/configuration/AuthenticationConfig.java
@@ -1,15 +1,27 @@
 package com.spring.sns.configuration;
 
+import com.spring.sns.configuration.filter.JwtTokenFilter;
+import com.spring.sns.exception.CustomAutenticationEntryPoint;
+import com.spring.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig {
+
+    private final UserService userService;
+
+    @Value("${jwt.secret-key}")
+    private String key;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,8 +34,11 @@ public class AuthenticationConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(ex -> {
-                });
+                })
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAutenticationEntryPoint());
 
         return http.build();
     }
---
+++ src/main/java/com/spring/sns/configuration/filter/JwtTokenFilter.java
@@ -0,0 +1,67 @@
+package com.spring.sns.configuration.filter;
+
+import com.spring.sns.model.User;
+import com.spring.sns.service.UserService;
+import com.spring.sns.util.JwtTokenUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final String key;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // get header
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(header == null || !header.startsWith("Bearer ")) {
+            log.error("Error occurs while getting header. header is null or invalid");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final String token = header.split(" ")[1].trim();
+
+            if (JwtTokenUtils.isExpired(token, key)) {
+                log.error("Key is expired");
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String userName = JwtTokenUtils.getUserName(token, key);
+            User user = userService.loadUserByUsername(userName);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    user, null, user.getAuthorities()
+            );
+            authentication.setDetails(new WebAuthenticationDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        } catch (RuntimeException e) {
+            log.error("Error occurs while validating. {}", e.toString());
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
