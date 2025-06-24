f3a8ce2ffb7187e0f5cf529cac166c7927f975ea
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-24 16:30:29 +0900

    feat: UserResponse 추가

---
+++ src/main/java/com/spring/sns/controller/response/UserResponse.java
@@ -0,0 +1,21 @@
+package com.spring.sns.controller.response;
+
+import com.spring.sns.model.User;
+import com.spring.sns.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponse {
+
+    private Integer id;
+    private String userName;
+
+    public static UserResponse fromUser(User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getUsername()
+        );
+    }
+}
