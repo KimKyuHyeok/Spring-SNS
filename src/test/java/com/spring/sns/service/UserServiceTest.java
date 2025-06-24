0ec436d11b08d65ee38901a123b47f97b0842dd3
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-24 15:40:31 +0900

    fix: User 관련 TC 수정

--- src/test/java/com/spring/sns/service/UserServiceTest.java
+++ src/test/java/com/spring/sns/service/UserServiceTest.java
@@ -48,7 +48,7 @@ public class UserServiceTest {
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
         when(encoder.encode(password)).thenReturn("encrypt_password");
-        when(userEntityRepository.save(any())).thenReturn(UserEntityFixture.get(userName, password));
+        when(userEntityRepository.save(any())).thenReturn(UserEntityFixture.get(userName, password, 1));
 
         Assertions.assertDoesNotThrow(() -> userService.join(userName, password));
     }
@@ -59,7 +59,7 @@ public class UserServiceTest {
         String userName = "userName";
         String password = "password";
 
-        UserEntity fixture = UserEntityFixture.get(userName, password);
+        UserEntity fixture = UserEntityFixture.get(userName, password, 1);
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
         when(userEntityRepository.save(any())).thenReturn(Optional.of(fixture));
@@ -74,7 +74,7 @@ public class UserServiceTest {
         String userName = "userName";
         String password = "password";
 
-        UserEntity fixture = UserEntityFixture.get(userName, password);
+        UserEntity fixture = UserEntityFixture.get(userName, password, 1);
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
         when(encoder.matches(password, fixture.getPassword())).thenReturn(true);
@@ -101,7 +101,7 @@ public class UserServiceTest {
         String password = "password";
         String wrongPassword = "wrongPassword";
 
-        UserEntity fixture = UserEntityFixture.get(userName, password);
+        UserEntity fixture = UserEntityFixture.get(userName, password, 1);
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
 
