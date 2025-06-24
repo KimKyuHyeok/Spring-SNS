ae58a7796827275dd10ab23784a8188116c5eee0
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-24 16:30:40 +0900

    feat: Post 수정 기능 구현

--- src/main/java/com/spring/sns/service/PostService.java
+++ src/main/java/com/spring/sns/service/PostService.java
@@ -2,6 +2,7 @@ package com.spring.sns.service;
 
 import com.spring.sns.exception.ErrorCode;
 import com.spring.sns.exception.SnsApplicationException;
+import com.spring.sns.model.Post;
 import com.spring.sns.model.entity.PostEntity;
 import com.spring.sns.model.entity.UserEntity;
 import com.spring.sns.repository.PostEntityRepository;
@@ -23,5 +24,25 @@ public class PostService {
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
 
         postEntityRepository.save(PostEntity.of(title, body, userEntity));
+    }
+
+    @Transactional
+    public Post modify(String title, String body, String userName, Integer postId) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        // post permission
+        if (postEntity.getUser() != userEntity) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission %s", userName, postId));
+        }
+
+        postEntity.setTitle(title);
+        postEntity.setBody(body);
+
+        return Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
 }
