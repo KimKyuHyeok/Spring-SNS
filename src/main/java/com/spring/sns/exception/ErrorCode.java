35f1fae38bc19a09c09fb9690d79bc1d81343f88
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-24 16:30:07 +0900

    feat: Post 관련 ErrorCode 추가

--- src/main/java/com/spring/sns/exception/ErrorCode.java
+++ src/main/java/com/spring/sns/exception/ErrorCode.java
@@ -9,12 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
-
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
-
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
-
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
+
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
     ;
