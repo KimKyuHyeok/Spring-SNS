67c9707386c0f4c8f0d0fc26cf3a799d3ea7b428
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-25 16:23:32 +0900

    feat: Post 삭제 및 조회 기능 구현

--- src/main/java/com/spring/sns/repository/PostEntityRepository.java
+++ src/main/java/com/spring/sns/repository/PostEntityRepository.java
@@ -1,9 +1,14 @@
 package com.spring.sns.repository;
 
 import com.spring.sns.model.entity.PostEntity;
+import com.spring.sns.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+
+    Page<PostEntity> findAllByUser(UserEntity entity, Pageable pageable);
 }
